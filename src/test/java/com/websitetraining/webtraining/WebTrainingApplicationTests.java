09b5c36ad4c6133ac687eb4022d71bdaf3e99c9d
GoddWilll <202543@umons.ac.be>
2023-02-15 17:33:42 +0100

    Project creation

---
+++ src/test/java/com/websitetraining/webtraining/WebTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websitetraining.webtraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
