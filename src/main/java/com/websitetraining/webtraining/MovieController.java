e08d98688f95f671b865aaa7d00b41b7e8695ebc
GoddWilll <202543@umons.ac.be>
2023-02-21 21:24:55 +0100

    Back end part

--- src/main/java/com/websitetraining/webtraining/MovieController.java
+++ src/main/java/com/websitetraining/webtraining/MovieController.java
@@ -8,20 +8,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.CrossOrigin; // for CORS
 import java.util.List;
 import java.util.Optional;
+
+
 
 @RestController
 @RequestMapping("/api/v1/movies") // any request to this uri will be handled by this controller
 public class MovieController {
     @Autowired
     private MovieService movieService;
+
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping
     public ResponseEntity<List<Movie>> getAllMovies(){
         return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/{imdbId}") // searching a movie by its id
     public ResponseEntity<Optional<Movie>>getSingleMovie(@PathVariable String imdbId){
         return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(imdbId), HttpStatus.OK);
--- src/main/java/com/websitetraining/webtraining/ReviewController.java
+++ src/main/java/com/websitetraining/webtraining/ReviewController.java
@@ -3,10 +3,7 @@ package com.websitetraining.webtraining;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -15,7 +12,7 @@ import java.util.Map;
 public class ReviewController {
     @Autowired
     private ReviewService reviewService;
-
+    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping // whatever we get, we want to convert it as a Map K,V.
     public ResponseEntity<Review> createReview(@RequestBody Map<String, String> payload){
         return new ResponseEntity<Review>(reviewService.createReview(payload.get("reviewBody"), payload.get("imdbId")), HttpStatus.CREATED);
